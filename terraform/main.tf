e65f9dec41462984a3737bfac43c878753d11a3f
Cormac Long <cormacjlong@gmail.com>
2024-04-01 16:20:09 +0100

    Update CDN custom domain configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,9 +73,9 @@ resource "azurerm_dns_cname_record" "cdn_dns_record" {
 
 # Add Custom Domain to CDN Endpoint
 resource "azurerm_cdn_endpoint_custom_domain" "cdn_custom_domain" {
-  name            = azurerm_dns_cname_record.cdn_dns_record.fqdn
+  name            = "${var.custom_url_prefix}-custom-domain"
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
-  host_name       = "${azurerm_dns_cname_record.cdn_dns_record.name}.${azurerm_dns_cname_record.cdn_dns_record.zone_name}"
+  host_name       = azurerm_dns_cname_record.cdn_dns_record.fqdn
   cdn_managed_https {
     certificate_type = "Dedicated"
     protocol_type    = "ServerNameIndication"
