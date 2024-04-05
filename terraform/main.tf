ffb79e8e8f7bc8f7098ebab93a373a83a403b91c
Cormac Long <cormacjlong@gmail.com>
2024-04-05 18:37:15 +0100

    Fix custom domain host name length in CDN endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -75,7 +75,7 @@ resource "azurerm_dns_cname_record" "cdn_dns_record" {
 resource "azurerm_cdn_endpoint_custom_domain" "cdn_custom_domain" {
   name            = "${var.custom_url_prefix}-custom-domain"
   cdn_endpoint_id = azurerm_cdn_endpoint.cdn_endpoint.id
-  host_name       = azurerm_dns_cname_record.cdn_dns_record.fqdn
+  host_name       = substr(azurerm_dns_cname_record.cdn_dns_record.fqdn, 0, length(azurerm_dns_cname_record.cdn_dns_record.fqdn) - 1)
   cdn_managed_https {
     certificate_type = "Dedicated"
     protocol_type    = "ServerNameIndication"
