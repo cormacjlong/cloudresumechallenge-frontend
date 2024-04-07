6d929a1bb6fc2c37bbac9e5400ec00af0d26296c
Cormac Long <cormacjlong@gmail.com>
2024-04-07 22:50:12 +0100

    Update default environment to "test" in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "project_prefix" {
 variable "env" {
   type        = list(string)
   description = "The environment currently being deployed."
-  default     = ["dev"]
+  default     = ["test"]
 }
 
 variable "custom_url_prefix" {
