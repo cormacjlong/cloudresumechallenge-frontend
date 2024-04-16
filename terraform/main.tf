5a21b15ab4e10c83f858433a3d176d60f314865f
Cormac Long <cormacjlong@gmail.com>
2024-04-16 19:39:46 +0100

    Refactor PowerShell command in main.tf to use the -File flag and update the script path

--- terraform/main.tf
+++ terraform/main.tf
@@ -98,6 +98,6 @@ resource "null_resource" "javascript_custom_domain" {
   }
 
   provisioner "local-exec" {
-    command = "pwsh ${self.triggers.script_path} -ApiGatewayUrl ${self.triggers.api_custom_url}"
+    command = "pwsh cd ../scripts && ${self.triggers.script_path} -ApiGatewayUrl ${self.triggers.api_custom_url}"
   }
 }
