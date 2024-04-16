64874c7116b6d15134d0a86ba34a2d1e4354ccbe
Cormac Long <cormacjlong@gmail.com>
2024-04-16 17:27:55 +0100

    Update relative path to updateApiGatewayUrl.ps1 in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -43,5 +43,5 @@ variable "logging_on" {
 variable "path_to_script_updateapigatwayurl" {
   type        = string
   description = "This is the relative path to the updateApiGatewayurl powershell script in the repo."
-  default     = "scripts/updateApiGatewayUrl.ps1"
+  default     = "../scripts/updateApiGatewayUrl.ps1"
 }
