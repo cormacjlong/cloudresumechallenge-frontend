032ee11139d40e263a26e8b041a65d09df058c15
Cormac Long <cormacjlong@gmail.com>
2024-04-19 17:48:24 +0100

    Fix relative path issue in updateAPIGatewayUrl.ps1 script and update API Gateway URL

--- scripts/updateAPIGatewayUrl.ps1
+++ scripts/updateAPIGatewayUrl.ps1
@@ -12,14 +12,14 @@ try{
     $jsScript = (Get-Content -Path $relativePathToJavascriptFile) -replace "const url = '.*?'", "const url = '$($ApiGatewayUrl)'"
 }
 catch {
-    Write-Output "An error occurred reading the file: $_"
+    Write-Host "An error occurred reading the file: $_"
 }
 
 try {
     # Modify the Javascript file.
     $jsScript | Set-Content -Path $relativePathToJavascriptFile
-    Write-Output "Updated the API Gateway URL in the Javascript file to: $ApiGatewayUrl"
+    Write-Host "Updated the API Gateway URL in the Javascript file to: $ApiGatewayUrl"
 }
 catch {
-    Write-Output "An error occurred writing to the file: $_"
+    Write-Host "An error occurred writing to the file: $_"
 }
