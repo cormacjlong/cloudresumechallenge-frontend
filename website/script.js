e26408cb2fb897d401b957ce9a015a6c353265ee
Cormac Long <cormacjlong@gmail.com>
2024-06-05 13:06:50 +0100

    Add spinner for visitor count

--- website/script.js
+++ website/script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const updateVisitorCount = async () => {
+        const spinner = document.getElementById('loadingSpinner');
         try {
-            const url = 'https://www.example.com/api';
+            const url = 'https://cv-api.az.macro-c.com/api/getvisitor';
             // Check if we already have a count for this session
             const storedCount = sessionStorage.getItem('currentVisitorCount');
             if (storedCount) {
@@ -9,7 +10,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 return; // Exit if we already have a count, avoiding an unnecessary API call
             }
             // If no count is stored, fetch it from the API
+            spinner.style.display = 'block'; // Show spinner
             const response = await fetch(url);
+            spinner.style.display = 'none'; // Hide spinner
             if (response.ok) {
                 const data = await response.text();
                 document.getElementById('visitorCount').textContent = data;
@@ -18,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.error('Failed to fetch visitor count:', response.statusText);
             }
         } catch (error) {
+            spinner.style.display = 'none'; // Hide spinner
             console.error('Error fetching visitor count:', error);
         }
     };
